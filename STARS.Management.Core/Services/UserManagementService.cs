2f83af33cfae7a984429624f9ae29b44b317b528
vaibhavjoshi888 <vaibhavjoshi888@gmail.com>
2022-09-30 05:46:50 -0700

    Dapper context added

--- STARS.Management.Core/Services/UserManagementService.cs
+++ STARS.Management.Core/Services/UserManagementService.cs
@@ -1,6 +1,39 @@
+using System.Collections.Generic;
+using STARS.Management.Core.DTO;
+using STARS.Management.Core.Interface;
+
 namespace STARS.Management.Core.Services;
 
-public class UserManagementService
+public class UserManagementService : IUserManagementService
 {
     
+    public void DeleteUser(string appuserid)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<UserDTO> GetAllUsers()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public UserDTO IsvalidUser(LogInDTO loginDTO)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SaveUser(UserDTO user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<UserDTO> SearchUser(string username)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateUser(string appuserid, UserDTO user)
+    {
+        throw new System.NotImplementedException();
+    }
 }
