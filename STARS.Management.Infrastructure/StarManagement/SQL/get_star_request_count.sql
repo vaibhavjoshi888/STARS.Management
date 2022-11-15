6c39ff8efd3f109a3cec9a61d80f0fe30a55c341
vaibhavjoshi888 <vaibhavjoshi888@gmail.com>
2022-11-15 11:22:36 +0530

    ssss

--- STARS.Management.Infrastructure/StarManagement/SQL/get_star_request_count.sql
+++ STARS.Management.Infrastructure/StarManagement/SQL/get_star_request_count.sql
@@ -1,6 +1,6 @@
 SELECT
-SUM(CASE WHEN Status = 'P' THEN 1 ELSE 0 END) AS Pending
+SUM(CASE WHEN Status IN ('P','A','D') THEN 1 ELSE 0 END) AS TotalSubmited
+,SUM(CASE WHEN Status = 'P' THEN 1 ELSE 0 END) AS Pending
 ,SUM(CASE WHEN Status = 'D' THEN 1 ELSE 0 END) AS Denied
 ,SUM(CASE WHEN Status = 'A' THEN 1 ELSE 0 END) AS Approved
-,SUM(CASE WHEN Status IN ('P','A','D') THEN 1 ELSE 0 END) AS TotalSubmitted
 FROM UserStarConfiguration
